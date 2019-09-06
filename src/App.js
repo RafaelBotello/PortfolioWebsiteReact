63e6f02592dcb26312d81e84769d4d7637b05f93
Rafael Botello <franciscobotello@Administrators-MacBook-Pro.local>
2019-09-06 10:40:19 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main></main>
+    </div>
+  );
+}
+
+export default App;

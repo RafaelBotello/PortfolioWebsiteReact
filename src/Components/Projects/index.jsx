6de4dd66bb19cef8b39f6ff47d8de9c43fa6935c
Rafael Botello <franciscobotello@Administrators-MacBook-Pro.local>
2019-12-09 13:21:00 -0500

    Added project description plus new pwa project to portfolio.

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -15,28 +15,35 @@ const Projects = () => {
           link="https://rafaelbotello.github.io/JerseyXecommerce-V1/product.html"
           imageUrl="https://files.slack.com/files-pri/T5VCY49B3-FPCVD54LW/image.png"
           imgDescription="Screenshot for Jersey X Ecommerce website"
-          projectDescription="Jersey X v1"
+          projectDescription="Jersey X v1(Semantic HTML/CSS)"
           descriptionClass="text-img"
         />
         <ProjectCont
           link="https://github.com/RafaelBotello/JerseyXecommerce-V2"
           imageUrl="https://files.slack.com/files-pri/T5VCY49B3-FPD09Q32S/image.png"
           imgDescription="Screenshot for Jersey X Ecommerce version 2 website"
-          projectDescription="Jersey X v2"
+          projectDescription="Jersey X v2(React)"
           descriptionClass="text-img"
         />
         <ProjectCont
           link="https://github.com/RafaelBotello/JerseyXecommerce-V3"
           imageUrl="https://files.slack.com/files-pri/T5VCY49B3-FNK4RD18B/image.png"
           imgDescription="Screenshot for Jersey X Ecommerce version 3 website"
-          projectDescription="Jersey X v3"
+          projectDescription="Jersey X v3(Databases/Node/JSON)"
           descriptionClass="text-img"
         />
         <ProjectCont
           link="https://github.com/RafaelBotello/JerseyXadminApp"
           imageUrl="https://github.com/RafaelBotello/JerseyXadminApp/blob/master/img/home.png?raw=true"
           imgDescription="Screenshot for Jersey X Admin website"
-          projectDescription="Jersey X Admin app"
+          projectDescription="Jersey X Admin app(c#)"
+          descriptionClass="text-img"
+        />
+        <ProjectCont
+          link="https://github.com/RafaelBotello/jerseyxPWA"
+          imageUrl="https://github.com/RafaelBotello/jerseyxPWA/blob/master/img/auditpwa.png?raw=true"
+          imgDescription="Screenshot for Jersey PWA"
+          projectDescription="Jersey X PWA (Web Accessibility)"
           descriptionClass="text-img"
         />
         {/* https://media.giphy.com/media/LMh3E7aMHaC0TjY0En/giphy.gif */}
